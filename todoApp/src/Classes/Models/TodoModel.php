2cc36b15cac8adcb30109f725624c0c03d72530a
Matt <matthew.tapley@hotmail.co.uk>
2018-11-21 13:56:48 +0000

    docblocked TodoModel.php

--- todoApp/src/Classes/Models/TodoModel.php
+++ todoApp/src/Classes/Models/TodoModel.php
@@ -9,16 +9,31 @@ class TodoModel
     private $db;
     private $todos = [];
 
+    /**
+     * TodoModel constructor.
+     *
+     * @param \PDO $db
+     */
     public function __construct(\PDO $db)
     {
         $this->db = $db;
     }
 
+    /**
+     * Adds a single $todo to the $todos array.
+     *
+     * @param string $todo
+     */
     public function displaySingleTodo(string $todo)
     {
         array_push($this->todos, $todo);
     }
 
+    /**
+     * Adds all $todos to the $todos array.
+     *
+     * @param array $todos
+     */
     public function displayAllTodos(array $todos)
     {
         foreach ($todos as $todo) {
