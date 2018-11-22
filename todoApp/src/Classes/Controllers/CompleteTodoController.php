a92742f42c9af5eb9bf0a079a3711ab99e4d0b38
Matt <matthew.tapley@hotmail.co.uk>
2018-11-22 13:23:21 +0000

    created CompleteTodoController.php

--- todoApp/src/Classes/Controllers/CompleteTodoController.php
+++ todoApp/src/Classes/Controllers/CompleteTodoController.php
@@ -2,7 +2,42 @@
 
 namespace Todo\Classes\Controllers;
 
+use \Slim\Http\Request as Request;
+use \Slim\Http\Response as Response;
+use Slim\Views\PhpRenderer;
+use Todo\Classes\Models\TodoModel;
+
 class CompleteTodoController
 {
+    private $renderer;
+    private $todoModel;
 
+    /**
+     * DisplayTodosController constructor.
+     *
+     * @param PhpRenderer $renderer
+     *
+     * @param TodoModel $todoModel
+     */
+    public function __construct(PhpRenderer $renderer, TodoModel $todoModel)
+    {
+        $this->renderer = $renderer;
+        $this->todoModel = $todoModel;
+    }
+
+    /**
+     *
+     *
+     * @param Request $request
+     * @param Response $response
+     * @param array $args
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function __invoke(Request $request, Response $response, array $args)
+    {
+        $data = $this->todoModel->completeTodo();
+        $args['data'] = $data;
+        return $this->renderer->render($response, 'display.phtml', $args);
+    }
 }
